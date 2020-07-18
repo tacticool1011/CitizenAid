38bf493e2bc5cd58932f6f9db9c45d383d4d95a0
larryz1230 <larryzhi1230@gmail.com>
2020-07-17 19:55:14 -0700

    nav bar set up

--- app/src/main/java/com/example/citizenaid/Activities/HomeActivity.java
+++ app/src/main/java/com/example/citizenaid/Activities/HomeActivity.java
@@ -1,4 +1,48 @@
 package com.example.citizenaid.Activities;
 
-public class HomeActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.citizenaid.R;
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitvity_home);
+
+
+
+        navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+
+        final NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+
+//        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                int id = menuItem.getItemId();
+//                if (id == R.id.home){
+////                    Toast.makeText(HomeActivity.this, "Home", Toast.LENGTH_SHORT).show();
+//                    startActivity(new Intent(getApplicationContext(), ConversationsActivity.class ));
+//                }
+//                else if (id == R.id.logout){
+////                    Toast.makeText(HomeActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+//                    startActivity(new Intent(getApplicationContext(), LoginActivity.class ));
+//                } else if (id == R.id.find_friends){
+//                    startActivity(new Intent(getApplicationContext(), FindFriendsActivity.class));
+//                }
+//                return true;
+//            }
+//        });
+    }
 }
