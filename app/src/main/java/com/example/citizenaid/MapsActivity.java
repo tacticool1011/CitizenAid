fdd74ea4cfb5dd999a87aa63323c5f1ab6c77a2c
Ryan <ryan@awkvi,com>
2020-07-18 21:23:22 -0700

    boi

--- app/src/main/java/com/example/citizenaid/MapsActivity.java
+++ app/src/main/java/com/example/citizenaid/MapsActivity.java
@@ -1,11 +1,14 @@
 package com.example.citizenaid;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -28,6 +31,7 @@ import com.example.citizenaid.LoginActivity;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Places;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -87,6 +91,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.)
 
         //checks if its a citizen of organization
         if (citizen.getEmail().equals("notcitizen")){
