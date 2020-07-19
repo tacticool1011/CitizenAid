0702b7baa97df2172ef0f481b3cc832a04286f2e
Zheng Andrew <andrewzheng04@gmail.com>
2020-07-19 11:48:04 -0700

    commented out

--- app/src/main/java/com/example/citizenaid/MapsActivity.java
+++ app/src/main/java/com/example/citizenaid/MapsActivity.java
@@ -118,6 +118,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //only one of these will be legit so you always have to check.
     private Citizen citizen = LoginActivity.getCitizen();
     private static Institutions institutions = LoginActivity.getInstitutions();
+    private Institutions soupKitchen, housing, donation;
     String userr;
     DrawerLayout d1;
     private static String URL_ADDCOOR = LoginActivity.ngrokID+"/userCoordinates/addcoordinates.php";
@@ -131,7 +132,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
+//        soupKitchen.addLocations(new Institution(soupKitchen, new LatLng(37.775592, -122.433313 ), "We provide food for the San Francisco, California area and provide a wide variety of extravagant cuisine" , "Momma's Food" , "Soup Kitchen"));
+//        soupKitchen.addLocations(new Institution(soupKitchen, new LatLng(40.644315, -73.957997) , "We provide food for the New York City, New York area and provide a wide variety of extravagant cuisine" , "Taste From Home" , "Soup Kitchen"));
+//        housing.addLocations(new Institution(housing, new LatLng(47.580100, -122.329141) , "We provide affordable housing units for the Seattle, Washington area" , "Affordable Housing First" , "Affordable Housing"));
+//        housing.addLocations(new Institution(housing, new LatLng(30.259667, -97.746062), "We provide affordable housing units for the Austin, Texas area" , "Texas Housing" , "Affordable Housing"));
+//        donation.addLocations(new Institution(donation, new LatLng(25.773600, -80.214817) , "We provide previously owned items ranging from toys to clothes for the financially unstable people in the Miami, Florida area" , "GoodWish" , "Donation"));
+//        donation.addLocations(new Institution(donation, new LatLng(33.446787, -112.077546) , "We provide previously owned items ranging from toys to clothes for the financially unstable people in the Phoenix, Arizona area" , "CitizenLove" , "Donation"));
         BitmapDrawable bitmap = (BitmapDrawable) getResources().getDrawable(R.drawable.institution_icon);
         Bitmap b = bitmap.getBitmap();
         institution_icon = Bitmap.createScaledBitmap(b, bitmapWidth, bitmapHeight, false);
@@ -393,6 +399,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        for(Institution i : soupKitchen.getLocations()){
+            LatLng institutePos = i.getPos();
+            mMap.addMarker(new MarkerOptions().position(institutePos).title("Soup Kitchen").icon(BitmapDescriptorFactory.fromBitmap(institution_icon)));
+        }
         removelocation = findViewById(R.id.remove);
         if(!(citizen.getEmail().equals("notcitizen"))){
             removelocation.setVisibility(View.GONE);
@@ -455,13 +465,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     @Override
                     public void onClick(View v) {
                         //marker.getPostition()
-                        if(description1 == null || type1 == null){
-                            Toast.makeText(MapsActivity.this, "Details not available", Toast.LENGTH_SHORT).show();
-                        } else {
+                       // if(description1 == null || type1 == null){
+                          //  Toast.makeText(MapsActivity.this, "Details not available", Toast.LENGTH_SHORT).show();
+                     //   } else {
                             startActivity(new Intent(MapsActivity.this, DetailsActivity.class));
                             finish();
                             return;
-                        }
+                        //}
                     }
                 });
 
